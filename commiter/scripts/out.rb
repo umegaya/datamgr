0855eccf366f09a26e3d05856459f2eecb84b713
Takehiro Iyatomi <iyatomi@gmail.com>
2016-04-11 14:52:03 +0900

    tweak for processing php's system() API spec

--- commiter/scripts/out.rb
+++ commiter/scripts/out.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/ruby
 
 curfd = nil
+oldln = nil
 while ln = STDIN.gets do
-        if ln =~ /^-+([\w]+)-+$/ then
+        if oldln and (oldln.chomp == ln.chomp) then
+            # avoid messy spec of php system() API call
+            # http://php.net/manual/ja/function.system.php
+        elsif ln =~ /^-+([\w]+)-+$/ then
                 table = $1
                 curfd.close if curfd
                 curfd = File.open(ARGV[0] + "/" + table + ".csv", "w+")
         elsif curfd then
                 curfd.puts ln.encode("UTF-8")
         end
+        oldln = ln
 end
